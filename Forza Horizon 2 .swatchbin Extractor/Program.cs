7c72a3a527ea12625273654ca02f8c924471fc7e
Rodrigo Todescatto <90097545+Rodrigo-Todescatto@users.noreply.github.com>
2024-11-11 20:31:10 -0300

    Add files via upload

--- Forza Horizon 2 .swatchbin Extractor/Program.cs
+++ Forza Horizon 2 .swatchbin Extractor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata;
 using XGXTYPES;
 using static XGXImports;
 
@@ -6,8 +7,7 @@ namespace FH2SW
 {
     public unsafe class Program
     {
-        public const string Version = "Test Build";
-        // (Version: {Version}) 
+        public const string Version = "Test 0.0.1";
         static void Main(string[] args)
         {
             Console.WriteLine("***************************************************");
@@ -15,6 +15,43 @@ namespace FH2SW
             Console.WriteLine("***************************************************");
             Console.WriteLine($"***************(Version: {Version})***************");
             Console.WriteLine("***************************************************");
+
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: Drag a .SWATCHBIN file");
+                return;
+            }
+
+            if (Directory.Exists(args[0]))
+            {
+                foreach (var file in Directory.GetFiles(args[0], "*.swatchbin", SearchOption.AllDirectories))
+                {
+                    try
+                    {
+                        ConvertSWATCHBINFILE(file);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine((ex.Message));
+                    }
+                }
+            }
+            else
+            {
+                ConvertSWATCHBINFILE(args[0]);
+            }
+        }
+
+        private static void ConvertSWATCHBINFILE(string file)
+        {
+            try
+            {
+                using var fs = File.OpenRead(file);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to load file! {ex.Message}");
+            }
         }
     }
 }
