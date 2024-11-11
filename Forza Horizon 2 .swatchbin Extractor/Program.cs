d3cbc159d68ab66b7548629c624f185028def1fb
Rodrigo Todescatto <umidiota090310@outlook.com>
2024-11-11 14:26:51 -0300

    Add project files.

---
+++ Forza Horizon 2 .swatchbin Extractor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using XGXTYPES;
+using static XGXImports;
+
+namespace FH2SW
+{
+    public unsafe class Program
+    {
+        public const string Version = "Test Build";
+        // (Version: {Version}) 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************************");
+            Console.WriteLine($"  Forza Horizon 2 .SWATCHBIN Extractor by Rod :D");
+            Console.WriteLine("***************************************************");
+            Console.WriteLine($"***************(Version: {Version})***************");
+            Console.WriteLine("***************************************************");
+        }
+    }
+}
---
+++ Forza Horizon 2 .swatchbin Extractor/XGXImporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using XGXTYPES;
+
+public partial class XGXImports
+{
+    [LibraryImport("xg_x.dll", EntryPoint = "XGCreateTexture1DComputer")]
+    public unsafe static partial int XGCreateTexture1DComputer(XG_TEXTURE1D_DESC* desc, XGTextureAddressComputer** computer);
+
+    [LibraryImport("xg_x.dll", EntryPoint = "XGCreateTexture2DComputer")]
+    public unsafe static partial int XGCreateTexture2DComputer(XG_TEXTURE2D_DESC* desc, XGTextureAddressComputer** computer);
+
+    [LibraryImport("xg_x.dll", EntryPoint = "XGCreateTexture3DComputer")]
+    public unsafe static partial int XGCreateTexture3DComputer(XG_TEXTURE3D_DESC* desc, XGTextureAddressComputer** computer);
+}
